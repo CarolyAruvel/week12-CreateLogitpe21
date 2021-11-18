81044a34d02d89b2d015c10e2e541b4a1f5a346a
Opilane TTHK <opilane@tthk.ee>
2021-11-18 10:24:12 +0200

    Week12

---
+++ CreateLogITpe21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CreateLogITpe21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string newDirectory = "LOGITpe21";
+
+            CreateMyDirectory(newDirectory);
+        }
+
+
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";          
+            string directoryfullPath = @$"{rootDirectory}\{myDirectoryName}";
+
+            if (Directory.Exists(directoryfullPath))
+            {
+                Console.WriteLine($"Directory {myDirectoryName} already exists in {rootDirectory}.");
+
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryfullPath);
+                Console.WriteLine($"Directory {myDirectoryName} has been created.");
+            }
+        }
+    }
+
+}
